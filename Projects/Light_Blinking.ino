511d762f91e611b9879dc0da274f25db2cab7105
Prajyot_Daphal <87372653+PrajyotDaphal@users.noreply.github.com>
2025-02-06 21:23:12 +0530

    Light_Blinking.ino

--- Projects/Light_Blinking.ino
+++ Projects/Light_Blinking.ino
@@ -2,6 +2,8 @@ Connections:
 GND = GND Pin
 // Before  LED positive is connecting to Arduino connect 1k resistor between arduino and LED connection  
 
+#include <Arduino.h>
+  
 void setup() {
   pinMode(13, OUTPUT); // Set pin 13 as an output
 }
